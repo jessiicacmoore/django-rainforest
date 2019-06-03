2c25418d7aa62eef9d095c933b235d20aa4248cd
Jessica Moore <jessica.cmoore1@gmail.com>
2019-06-03 17:01:20 -0400

    Registered product model with admin

--- rainforest/models.py
+++ rainforest/models.py
@@ -5,3 +5,6 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price_in_cents = models.IntegerField()
+
+    def __str__(self):
+        return "{} - ${}".format(self.name, self.price_in_cents)
